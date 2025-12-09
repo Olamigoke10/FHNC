8f6e37071dbc6d2d33d02e2eb63d04cf5cec0f70
MRBOX <olamigokeadebayo90@gmail.com>
2025-12-09 13:27:06 +0000

    New Logo

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -45,9 +45,9 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-20">
           {/* Logo */}
-          <div className="flex-shrink-0 w-14 md:w-16 shadow-md rounded-full overflow-hidden">
+          <div className="flex-shrink-0 w-14 md:w-16 ">
             <NavLink to="/" className="flex items-center">
-              <img src={logo} alt="Logo" className=" rounded-full font-extrabold" />
+              <img src={logo} alt="Logo" className=" font-extrabold" />
             </NavLink>
           </div>
 
